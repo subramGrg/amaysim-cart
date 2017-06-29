643736e1e984d4ae685bcfb21475cf09633365bd
Subram Gurung <subram@i-motor.com.au>
2017-06-29 16:15:20 +1000

    organised the order of classes

--- lib/supa_roo.rb
+++ lib/supa_roo.rb
@@ -1,8 +1,8 @@
 module SupaRoo
 	autoload :ShoppingCart, 'shopping_cart'
-	autoload :LargeSim, "sims/large_sim.rb"
-	autoload :MediumSim, "sims/medium_sim.rb"
-	autoload :OneGb, "sims/one_gb.rb"
 	autoload :Sim, "sims/sim.rb"
 	autoload :SmallSim, "sims/small_sim.rb"
+	autoload :MediumSim, "sims/medium_sim.rb"
+	autoload :LargeSim, "sims/large_sim.rb"
+	autoload :OneGb, "sims/one_gb.rb"
 end

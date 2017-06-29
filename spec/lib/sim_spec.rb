59dc304f907eddcee3db7c5f14ad00e091d3ba2b
Subram Gurung <subram@i-motor.com.au>
2017-06-29 12:05:57 +1000

    added rspec

---
+++ spec/lib/sim_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'shopping_cart'
+
+require 'sims/sim'
+require 'sims/small_sim'
+
+
+RSpec.describe ShoppingCart do
+	describe "ShoppingCart" do
+		it "works"
+	end
+end

d0a0c391d283292ea15492ae9ae42ec7f09bdfec
Subram Gurung <subram@i-motor.com.au>
2017-06-29 16:34:33 +1000

    added test to check refactored MediumSim

--- spec/lib/sim_spec.rb
+++ spec/lib/sim_spec.rb
@@ -7,9 +7,14 @@ RSpec.describe SupaRoo do
       expect(subject.to_s).to be_truthy
 		end
 
-		it "adds item to cart" do
+		it "adds small sim to cart" do
 			subject.add :ut_small, 4
 			expect(subject.to_s).to eql(99.6)
+		end
+
+		it "adds medium sim to cart" do
+			subject.add :ut_medium, 4
+			expect(subject.to_s).to eql(119.6)
 		end
 	end
 end

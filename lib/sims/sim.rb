ef5811096d560f94d44277bd2a7d6520d7744192
Subram Gurung <subram@i-motor.com.au>
2017-07-14 13:14:46 +1000

    cleaned lib structure for new pattern

--- lib/sims/sim.rb
+++
@@ -1,9 +0,0 @@
-module SupaRoo
-  Sim = Struct.new(:quantity, :total) do
-    attr_accessor :total, :price, :quantity
-
-    def calculate_price
-      self.total = price * quantity
-    end
-  end
-end
--- lib/supa_roo.rb
+++
@@ -1,8 +0,0 @@
-module SupaRoo
-	autoload :ShoppingCart, 'shopping_cart'
-	autoload :Sim, "sims/sim.rb"
-	autoload :SmallSim, "sims/small_sim.rb"
-	autoload :MediumSim, "sims/medium_sim.rb"
-	autoload :LargeSim, "sims/large_sim.rb"
-	autoload :OneGb, "sims/one_gb.rb"
-end
